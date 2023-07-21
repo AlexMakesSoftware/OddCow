c2d2772fc4d6c220b3c0aa23848a93805afa8631
Alex Connell <83471521+AlexMakesSoftware@users.noreply.github.com>
2023-07-21 16:40:03 +0100

    user reg

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import RegisterUserForm
+
 
 def login_user(request):
     if request.method == "POST":
@@ -18,7 +20,24 @@ def login_user(request):
     else:
         return render(request, 'authenticate/login.html', {})
     
+
 def logout_user(request):
     logout(request)
     messages.success(request, 'You were logged out.')
     return redirect('login')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request, user)
+            messages.success(request, ("Registration sucessful."))
+            return redirect('home')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'authenticate/register_user.html',{'form':form})
