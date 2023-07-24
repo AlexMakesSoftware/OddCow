2de616f7ceb2db2e7331fd05b7931ce590b77f9e
Alex Connell <83471521+AlexMakesSoftware@users.noreply.github.com>
2023-07-24 08:39:24 +0100

    correct permissions string

--- accounts/access_controller.py
+++ accounts/access_controller.py
@@ -1,7 +1,6 @@
-# custom_middleware.py
-
 from django.http import HttpResponseForbidden, HttpResponseRedirect
 from django.urls import reverse
+
 
 class AccessControlMiddleware:
     def __init__(self, get_response):
@@ -14,25 +13,27 @@ class AccessControlMiddleware:
     def process_view(self, request, view_func, view_args, view_kwargs):
         # List of URLs that don't require privilege checks
         exempt_urls = [
-            reverse('login'),  # Replace 'login' with the name of your login view URL
+            reverse('login'), 
             reverse('register_user'),
             reverse('logout'),
             reverse('unauthorised_page'),  # A page to send unauthorised people to.
-            # Add more exempt URLs as needed
         ]
 
         # If the user is not logged in
         if not request.user.is_authenticated:
+            # print("User not authenticated")
             # Check if the requested URL is in the exempt URLs list
             if not any(request.path_info.startswith(url) for url in exempt_urls):
                 # Redirect unauthenticated users to the login page
                 return HttpResponseRedirect(reverse('login'))  # Replace 'login' with the actual login URL name
 
         # If the user is authenticated but doesn't have the required permission
-        elif not request.user.has_perm('approved_user'):
+        elif not request.user.has_perm('auth.approved_user'):
+            # print("User authenticated but doesn't have approval.")            
             # Check if the requested URL is in the exempt URLs list
             if not any(request.path_info.startswith(url) for url in exempt_urls):
                 # Redirect authenticated users without the special permission to a static page
                 return HttpResponseRedirect(reverse('unauthorised_page'))  # Replace with your static page URL
-
+        # else:
+            # print("Authorised")
         return None
